02d5785f43451232a1efebf3911aa4f88d82a710
xRokz <38587835+xrokz@users.noreply.github.com>
2020-04-21 20:05:42 +0300

    theme

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function setDark() {
-    document.querySelector("").setAttribute("data-theme", "dark");
+    document.querySelector("html").setAttribute("data-theme", "dark");
     document.querySelector("#dark").style.display = null;
     document.querySelector("#light").style.display = "block";
 }
 
 function setLight() {
-    document.querySelector("").setAttribute("data-theme", "light");
+    document.querySelector("html").setAttribute("data-theme", "light");
     document.querySelector("#light").style.display = null;
     document.querySelector("#dark").style.display = "block";
 
